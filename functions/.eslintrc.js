fe1b1d3dd50d6b265e341852b6043da9a83de276
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-03-11 21:26:20 +0900

    send message to slack

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -24,5 +24,6 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
+    "max-len": ["error", 130],
   },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,17 @@
 import * as functions from "firebase-functions";
+import axios from "axios";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+export const helloWorld = functions.https.onRequest(async (request: functions.https.Request, response: functions.Response) => {
+  const slackWebhookUrl = process.env.SLACKWEBHOOKURL!;
+  await axios.post(slackWebhookUrl, {
+    text: "hello gucchi!",
+  }).then(() => {
+    functions.logger.info("complete send message to slack");
+  }).catch(() => {
+    functions.logger.info("failed send message to slack");
+  });
   response.send("Hello gucchi!");
 });
