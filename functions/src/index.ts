49d22d72170d3b1767437b17d59c07d581446bef
yamoyamoto <a18sb009@zr.osaka-cu.ac.jp>
2022-03-15 20:51:29 +0900

    firestore sample

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 import axios from "axios";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
+
+const fireStore = new admin.firestore.Firestore();
 
 export const helloWorld = functions.https.onRequest(async (request: functions.https.Request, response: functions.Response) => {
   const slackWebhookUrl = process.env.SLACKWEBHOOKURL!;
@@ -13,5 +16,19 @@ export const helloWorld = functions.https.onRequest(async (request: functions.ht
   }).catch(() => {
     functions.logger.info("failed send message to slack");
   });
+
+  const correctionRef = fireStore.collection("sample_correction");
+
+  const document = await correctionRef.add({
+    title: "title",
+    body: "hello world",
+  });
+
+  await document.update({
+    body: "My first Firestore app",
+  });
+
+  // const doc = document.get();
+
   response.send("Hello gucchi!");
 });
